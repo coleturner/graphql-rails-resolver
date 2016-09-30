16120604ac547bfe9557e133f04e78e7d95f6d33
Cole Turner <turner.cole@gmail.com>
2016-09-30 10:58:53 -0400

    Removed debug info

--- lib/graphql/rails/resolver.rb
+++ lib/graphql/rails/resolver.rb
@@ -1,7 +1,7 @@
 module GraphQL
   module Rails
     class Resolver
-      VERSION = '0.1.5'
+      VERSION = '0.1.6'
 
       attr_accessor :resolvers
 
@@ -86,7 +86,6 @@ module GraphQL
         end
 
         result = payload
-        puts "Payload = #{result}"
 
         @obj = nil
         @args = nil
